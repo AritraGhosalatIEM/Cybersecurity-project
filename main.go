bb8ca448451457b209464bca0b1d91267973abea
AritraGhosalatIEM <119795273+AritraGhosalatIEM@users.noreply.github.com>
2025-03-24 23:31:20 +0530

    GUI skeleton
    Basic GUI skeleton and basic input boxes added

--- main.go
+++ main.go
@@ -6,6 +6,16 @@ import (
 	"crypto/rand"
 	"fmt"
 	"io"
+	"os"
+	// "reflect"
+	"image/color"
+	"gioui.org/app"
+	"gioui.org/op"
+	// "gioui.org/text"
+	"gioui.org/unit"
+	"gioui.org/widget/material"
+	"gioui.org/widget"
+	"gioui.org/layout"
 )
 func pad(data []byte, blockSize int) []byte {
 	padding := blockSize - len(data)%blockSize
@@ -14,6 +24,133 @@ func pad(data []byte, blockSize int) []byte {
 	//PKCS7 Convention
 	padText:= bytes.Repeat(add_byte,padding)
 	return append(data,padText...)
+}
+var key_label material.LabelStyle;
+var key_input [24]widget.Editor
+func show_encryption(){
+	key1:=make([]byte,8)
+	key2:=make([]byte,8)
+	key3:=make([]byte,8)
+	for i:=0;i<8;i++{
+		if key_input[i].Len()!=1 || key_input[i+8].Len()!=1 || key_input[i+16].Len()!=1{
+			key_label.Color=color.NRGBA{R:255,G:0,B:0,A:255}
+			key_label.Text="Full Key not entered"
+			return
+		}
+		key1[i]=key_input[i].Text()[0]
+		key2[i]=key_input[i+8].Text()[0]
+		key3[i]=key_input[i+16].Text()[0]
+	}
+	key_label.Color=color.NRGBA{R:0,G:0,B:0,A:255}
+	key_label.Text="KYS"
+	fmt.Println(string(key1))
+	fmt.Println(string(key2))
+	fmt.Println(string(key3))
+}
+func window(){
+	win:= new(app.Window)
+	win.Option(app.Title("3DES-CBC"))
+	var ops op.Ops
+	var encrypt_button widget.Clickable
+	var text_input widget.Editor
+	theme:=material.NewTheme()
+	key_label=material.Label(theme,20.0,"")
+	for i:=0;i<24;i++{
+		key_input[i].MaxLen=1
+	}
+	for {
+		switch typ:=win.Event().(type){
+			case app.FrameEvent:
+				var gtx layout.Context=app.NewContext(&ops,typ)
+				if encrypt_button.Clicked(gtx){
+					show_encryption()
+				}
+				layout.Flex{
+					Axis:layout.Vertical,
+					Spacing:layout.SpaceBetween,
+				}.Layout(gtx,
+					layout.Rigid(//input section
+						func(gtx layout.Context) layout.Dimensions{
+							return layout.Flex{
+								Axis:layout.Horizontal,
+								Spacing:layout.SpaceBetween,
+							}.Layout(gtx,
+								layout.Rigid(
+									func(gtx layout.Context) layout.Dimensions{
+										return layout.Flex{
+											Axis:layout.Vertical,
+											Spacing:layout.SpaceBetween,
+										}.Layout(gtx,
+											layout.Rigid(
+												func(gtx layout.Context) layout.Dimensions{
+													return layout.Flex{
+														Axis:layout.Horizontal,
+														Spacing:layout.SpaceBetween,
+													}.Layout(gtx,
+														layout.Rigid(
+															func(gtx layout.Context) layout.Dimensions{
+																return material.Label(theme,20.0,"Text : ").Layout(gtx)
+															},
+														),
+														layout.Rigid(
+															func(gtx layout.Context) layout.Dimensions{
+																return material.Editor(theme,&text_input,"").Layout(gtx)
+															},
+														),
+													)
+												},
+											),
+											layout.Rigid(
+												func(gtx layout.Context) layout.Dimensions{
+													var elements [25] layout.FlexChild//one label and 24 input boxes
+													elements[0]= layout.Rigid(//label
+														func(gtx layout.Context) layout.Dimensions{
+															return material.Label(theme,20.0,"Key : ").Layout(gtx)
+														},
+													)
+													for i:=1;i<25;i++{//make input boxes
+														elements[i]=layout.Rigid(
+															func(gtx layout.Context) layout.Dimensions{
+																return widget.Border{
+																	Color: color.NRGBA{R:0,G:0,B:0,A:255},
+																	Width: unit.Dp(1),
+																}.Layout(gtx,
+																	func(gtx layout.Context) layout.Dimensions{
+																		return material.Editor(theme,&key_input[i-1]," ").Layout(gtx)
+																	},
+																)
+															},
+														)
+													}
+													return layout.Flex{
+														Axis:layout.Horizontal,
+														Spacing:layout.SpaceBetween,
+													}.Layout(gtx, elements[:]...,)
+												},
+										),
+									)
+									},
+								),
+								layout.Rigid(
+									func(gtx layout.Context) layout.Dimensions {
+										button:=material.Button(theme,&encrypt_button,"Encrypt")
+										return button.Layout(gtx)
+									},
+								),
+							)
+						},
+					),
+					layout.Rigid(
+						func(gtx layout.Context) layout.Dimensions{
+							return key_label.Layout(gtx)
+						},
+					),
+				)
+				typ.Frame(gtx.Ops)
+			case app.DestroyEvent:
+				os.Exit(0)
+		}
+	}
 }
 func main() {
 	// 24-byte key for Triple DES (3DES)
@@ -66,4 +203,6 @@ func main() {
 	}
 	fmt.Printf("Decrypted: %x\n",original)
 	fmt.Printf("Original text : %s\n",original[des.BlockSize:length-int(original[length-1])])
+	go window()
+	app.Main()
 }
